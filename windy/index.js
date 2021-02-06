76defde9a199f3ea375ce045f61537605132f198
karuiner <intaekgong@gmail.com>
2021-02-06 15:58:32 +0900

    update -21/02/06/15/58-

--- windy/index.js
+++ windy/index.js
@@ -1,1 +1,13 @@
 const login_page = document.querySelector(".login_page");
+const login_windows = document.querySelectorAll(".login_window");
+const [login_window_main, login_window_resister, login_window_sub] = login_windows;
+
+login_window_main.querySelector(".button_l").addEventListener("click", function () {
+    login_window_main.classList.replace("visible", "invisible");
+    login_window_resister.classList.replace("invisible", "visible");
+});
+
+login_window_resister.querySelector(".button_l").addEventListener("click", function () {
+    login_window_main.classList.replace("invisible", "visible");
+    login_window_resister.classList.replace("visible", "invisible");
+});

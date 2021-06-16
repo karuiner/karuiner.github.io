e1526d7559efecd66b366a7aefa26ce929722d9d
karuiner <intaekgong@gmail.com>
2021-06-16 21:24:52 +0900

    게시판 형태 잡기 (1)

--- home/src/main/index.tsx
+++ home/src/main/index.tsx
@@ -20,6 +20,10 @@ const Tbox = styled.div`
   font-size: 50px;
 `;
 
+const Stbox = styled.div`
+  display: flex;
+`;
+
 const BoardFrame = styled.div`
   flex: 1 0 0;
   width: 100%;
@@ -28,6 +32,54 @@ const BoardFrame = styled.div`
 const NumberBox = styled.div`
   height: 50px;
   width: 100%;
+  display: flex;
+  border-top: solid 1px white;
+  justify-content: center;
+  align-items: center;
+`;
+const NumberLinkBox = styled.div`
+  display: flex;
+  height: 95%;
+  width: 80%;
+`;
+const LinkBox = styled.div`
+  height: 40px;
+  width: 40px;
+  color: white;
+`;
+
+const LabelBox = styled.div`
+  height: 50px;
+  width: 100%;
+  border-top: solid 1px white;
+  border-bottom: solid 1px white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LabelInnerBox = styled.div`
+  display: flex;
+  height: 95%;
+  width: 95%;
+`;
+const Number = styled.div`
+  display: flex;
+  flex: 1 0 0;
+  justify-content: center;
+  align-items: center;
+`;
+const Subject = styled.div`
+  display: flex;
+  flex: 8 0 0;
+  justify-content: center;
+  align-items: center;
+`;
+const Date = styled.div`
+  display: flex;
+  flex: 2 0 0;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Main = function () {
@@ -36,8 +88,31 @@ const Main = function () {
       <Title>
         <Tbox>타이틀</Tbox>
       </Title>
+      <LabelBox>
+        <LabelInnerBox>
+          <Number>
+            <Stbox>번호</Stbox>
+          </Number>
+          <Subject>
+            <Stbox>제목</Stbox>
+          </Subject>
+          <Date>
+            <Stbox> 날짜</Stbox>
+          </Date>
+        </LabelInnerBox>
+      </LabelBox>
       <BoardFrame></BoardFrame>
-      <NumberBox></NumberBox>
+      <NumberBox>
+        <NumberLinkBox>
+          <LinkBox>이전</LinkBox>
+          {Array(10)
+            .fill(1)
+            .map((x, i) => {
+              return <LinkBox key={i}>{i < 5 ? `${i + 1}` : ""}</LinkBox>;
+            })}
+          <LinkBox>이후</LinkBox>
+        </NumberLinkBox>
+      </NumberBox>
     </Frame>
   );
 };

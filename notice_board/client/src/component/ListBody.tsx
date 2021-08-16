7913f23b1c6b8ea03ce0c9c2527770ec218cd46e
karuiner <intaekgong@gmail.com>
2021-08-16 19:55:53 +0900

    기본 구조 진핸

--- notice_board/client/src/component/ListBody.tsx
+++ notice_board/client/src/component/ListBody.tsx
@@ -1,25 +1,37 @@
 import React from "react";
 import styled from "styled-components";
+import Box from "./ListBox";
 
 const Frame = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
   flex: 1 0 0;
+  padding: 10px;
   flex-direction: column;
 `;
 
 const Header = styled.div`
   min-height: 50px;
-  display: felx;
+  display: flex;
   flex: 1 0 0;
   border: solid red 1px;
+  margin-top: 5px;
+  margin-left: 5px;
+  margin-right: 5px;
+  padding: 5px;
 `;
 
 const Body = styled.div`
   min-height: 500px;
   display: flex;
   flex: 10 0 0;
+  border: solid black 1px;
+  flex-direction: column;
+  margin-bottom: 5px;
+  margin-left: 5px;
+  margin-right: 5px;
+  padding: 5px;
 `;
 
 const Footer = styled.div`
@@ -27,13 +39,23 @@ const Footer = styled.div`
   display: felx;
   flex: 1 0 0;
   border: solid blue 1px;
+  margin: 5px;
 `;
 
 function ListBody() {
+  let dummy = Array(100).fill(0);
   return (
     <Frame>
-      <Header></Header>
-      <Body>{}</Body>
+      <Header>
+        <Box num={"번호"} sub={"제목"} date={"작성일자"}></Box>
+      </Header>
+      <Body>
+        {dummy.slice(0, 10).map((x, i) => {
+          return (
+            <Box key={i} num={i + 1} sub={`dummy${i + 1}`} date={"test"}></Box>
+          );
+        })}
+      </Body>
       <Footer></Footer>
     </Frame>
   );
--- notice_board/client/src/component/ListBox.tsx
+++ notice_board/client/src/component/ListBox.tsx
@@ -1,13 +1,60 @@
 import React from "react";
 import styled from "styled-components";
 
-const Frame = styled.div<{ height: number }>`
-  height: ${({ height }) => height}px;
-  width: 100%;
+const Frame = styled.div`
+  display: flex;
+  flex: 1 0 0;
+  margin: 5px;
+  border: solid red 1px;
 `;
 
-function ListBox(height: number) {
-  return <Frame height={height}></Frame>;
+const Num = styled.div`
+  display: flex;
+  flex: 1 0 0;
+  justify-content: center;
+  align-items: center;
+  border: solid black 1px;
+`;
+
+const Subject = styled.div`
+  display: flex;
+  flex: 5 0 0;
+  justify-content: center;
+  align-items: center;
+  border: solid black 1px;
+`;
+
+const Date = styled.div`
+  display: flex;
+  flex: 2 0 0;
+  justify-content: center;
+  align-items: center;
+  border: solid black 1px;
+`;
+
+const Tbox = styled.div`
+  display: flex;
+`;
+interface box {
+  num?: number | string;
+  sub?: string;
+  date?: string;
+}
+
+function ListBox({ num, sub, date }: box) {
+  return (
+    <Frame>
+      <Num>
+        <Tbox>{num ? num : ""} </Tbox>
+      </Num>
+      <Subject>
+        <Tbox>{sub ? sub : ""}</Tbox>
+      </Subject>
+      <Date>
+        <Tbox>{date ? date : ""}</Tbox>
+      </Date>
+    </Frame>
+  );
 }
 
 export default ListBox;
